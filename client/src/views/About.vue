1a71189124b8e5979ad2b024dfa1d3cfd88d6053
KangLeian <rayhannabhanlukmansyah@gmail.com>
2020-06-26 06:30:07 +0700

    menambah component winner board

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <WinnerBoard/>
   </div>
 </template>
+
+<script>
+import WinnerBoard from '@/components/WinnerBoard.vue'
+
+export default {
+  name: 'about',
+  components: {
+    WinnerBoard,
+  }
+}
+</script>
